ef9b7988703384a169050cce9e77b0f22d301b11
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-01-30 00:55:35 +0500

    upload chapter 2

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
-// ~~~~~ chapter 1 ~~~~~~~
-
-//  ****QUESTION 1***//
-// alert("hello, sir")
-
-// ***Question 2***//
-// alert("Thankyou! For Coming \n My Web page")
-
-//  ****QUESTION 3***//
-// alert("*WELCOME TO MY WEB PAGE* \n happy coding!!!! ")
-
-// ***QUESTION 4****//
-// alert("WELCOME TO JS LAND...")
-// alert("HAPPY CODING !!!")
-
-// ***QUESTION 5****//
-// alert("Hello... I can run JS through my web browser's cansole")
-
-// ***QUESTION 6 ***//
-// alert("My new web using html & css")
+//*** chapter 2****//
 
 
-//QUESTION 7 ***//
-alert("following project")
+//'''Question 1'''//
+// var userName ="test";
+// console.log(userName)
 
 
+//''Question 2''//
+// var myName ="muhammad ahmed";
+// console.log(myName)
 
+
+//''Question 3''//
+// var message ="Hello world";
+// alert(message)
+
+
+//''Question 4''//
+// var name = "Ahmed";
+// var age = "21 years old";
+// var qualification = "certified Hafiz-e-Quaraan";
+// alert(name)
+// alert(age)
+// alert(qualification)
+
+
+//''Question 5''//
+// var pizza = "pizza \npizz \npiz \npi \np";
+// alert(pizza)
+
+//''Question 6''// 
+// var email = "my email address is m.ahmedkarim@gmail.com";
+// alert(email)
+
+
+//''Question 7''//
+// var book = "“A smarter way to learn JavaScript”.";
+// alert ("I am trying to learn from the book"+book)
+
+
+//''Question 8''//
+// document.write("Yah! I can write HTML content through Javascrip");
+
+
+//''Question 9''//
+// var content = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert (content)
