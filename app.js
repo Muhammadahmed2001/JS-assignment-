501f892a40764de3ef73e2291996769437fb298c
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-02-04 02:30:50 +0500

    upload chapter 5

--- app.js
+++ app.js
@@ -1,49 +1,237 @@
-//..chapter 4..//
+//..chapter 5..//
+//..question 1..//
+
+var num1 = 3
+var num2 = 5
+// var sum = num1 + num2
+// document.write("sum of"+" " + num1 + "and" +" " + num2+" " + "is" + " "+sum);
 
 
-//..Question 1 ..//
-// var name,father,age = "my name is ahmed ";
+//..question 2..//
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 - num2
+// document.write("subtraction of"+" " + num1 + "and" +" " + num2+" " + "is" + " "+sum);
+
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 * num2
+// document.write("multiplication of"+" " + num1 + "and" +" " + num2+" " + "is" + " "+sum);
+
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 / num2
+// document.write("division of"+" " + num1 + "and" +" " + num2+" " + "is" + " "+sum);
 
 
-//..Question 2..//
-//..Illegal variable..//
-// var 2nd = "ahmed";
-// var father name ="a.karim";
-// var var ="my var";
-// var ahmedkarim = "my name";
-// var Rose = "gulab";
-
-// //..Legal variable..//
-// var name ="ahmed";
-// var ahmedKarim ="my name";
-// var _age ="21 years old";
-// var $value = "250 rupes";
-// var x = "saylani";
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 % num2
+// document.write("division of"+" " + num1 + "and" +" " + num2+" " + "is" + " "+sum);
 
 
-//..Question 3..//
 
-//..A..//
-// document.write("<h1>Rules for naming JS variables</h1>");
 
-//..B..//
-// var $ = "$";
-// var 1 = "1st";
-// var _ = "variable";
-
+//..question 3..//
+//..a-b..///
+// var num1 = "";
+// document.write("Value after variable declaration is" + " "+ num1);
 
 //..c..//
-// var $name = "ahmed";
-// var _name = "ahmed";
-// var name = "ahmed";
-
+// var num2 = 10;
 
 //..d..//
-// var js = "sensitive";
+var num3 = 5;
+// document.write("Initial value" + " " +num3);
+
+//..e-f..//
+var num4 = num3++;
+// document.write("Value after increment is" + " " + num3);
+
+//..g..//
+var num4 = num3 + 7;
+
+//..h..//
+// document.write("Value after addition is" + " " + num4 );
+
+//..i..//
+var num5 = num4--;
 
 
-//..e..//
-// var keywords = "js";
+//..j..//
+// document.write("Value after decrement is" + " " + num4);
+
+//..k-l..//
+var num6 = num4 % 3;
+// document.write("The remainder is"+ " "+num6);
+
+// //..question 4..//
+// var ticket = 600;
+// var sum = ticket * 5
+// document.write( "Example output " + sum) ;
+
+
+//..question 5..//
+// document.write('<h1> Table of 7 </h1>')
+// var aa = 1
+// var ab = 2
+// var ac = 3
+// var ad = 4
+// var ae = 5
+// var af = 6
+// var ag = 7
+// var ah = 8
+// var ai = 9
+// var aj = 10
+
+// var ta = ag * aa
+// document.write(ab + "x" + aa + "=" + ta + "<br />")
+
+// var ta = ag * ab
+// document.write(ab + "x" + ab + "=" + ta + "<br />")
+
+// var ta = ag * ac
+// document.write(ab + "x" + ac + "=" + ta + "<br />")
+
+// var ta = ag * ad
+// document.write(ab + "x" + ad + "=" + ta + "<br />")
+
+// var ta = ag * ae
+// document.write(ab + "x" + ae + "=" + ta + "<br />")
+
+// var ta = ag * af
+// document.write(ab + "x" + af + "=" + ta + "<br />")
+
+// var ta = ag * ag
+// document.write(ab + "x" + ag + "=" + ta + "<br />")
+
+// var ta = ag * ah
+// document.write(ab + "x" + ah + "=" + ta + "<br />")
+
+// var ta = ag * ai
+// document.write(ab + "x" + ai + "=" + ta + "<br />")
+
+// var ta = ag * aj
+// document.write(ab + "x" + aj + "=" + ta + "<br />")
+
+
+//..question 6..//
+// var celsius = 27
+// var convertTofehranheit = (27 * 9 / 5) + 32
+
+// var fehranheit = 81
+// var convertTocelsius = (81 - 32) * 5 / 9
+
+// document.write(celsius+"<sup>0</sup>C" + " " + "is"+" "+ convertTofehranheit  +"<sup>o</sup>F <br />")
+// document.write(fehranheit+"<sup>0</sup>F" + " " + "is"+" "+ convertTocelsius  +"<sup>o</sup>C")
+
+
+// ..question 7..//
+// var item1 = 650
+// var item2 = 100
+// var charges = 100
+
+// //..a..//
+// document.write("Price of item 1 is" + " " + item1 + "<br />")
+
+// //..b..//
+// var item1Total = item1 * 3
+// document.write("Quantity of item 1 is 3" + "<br />")
+
+// //..c..//
+// document.write("Price of item 2 is" + " " + item2 + "<br />")
+
+// //..d..//
+// var item2Total = item2 * 7 
+// document.write("Quantity of item 2 is 7" + "<br />")
+
+// //..e..//
+// document.write("Shipping charges" + " "+ charges +"<br />" + "<br />" )
+
+// var total = charges + item1Total + item2Total
+// document.write("Total cost of your order is " + " "+ total)
+
+
+
+//..question 8 ..//
+// var totalMarks = 980
+// var marksObtained = 804
+// var result = marksObtained * 100 / totalMarks
+// document.write("<h1> Marks sheet </h1>")
+// document.write("Total marks :" + " "+ totalMarks + "<br />")
+// document.write("Marks obtained :" + " "+ marksObtained + "<br />")
+// document.write("Percentage :" + " "+ result + "<br />")
+
+
+
+//..question 9 ..//
+// var usd = 104.80
+// var riyal = 28
+
+// total = usd * 10 + riyal * 25
+// document.write("Total currency in PKR :" + " "+ total)
+
+
+
+//..question 10..//
+// var num = 5 + 5 * 10 / 2
+// document.write(num)
+
+
+
+//..question 11..//
+// var current = 2023
+// var birth = 2001
+// document.write("<h1> Age Calculator </h1>")
+// document.write("current Year :" + " " + current + "<br />")
+// document.write("Birth year :" + " " + birth + "<br />")
+
+// var age = current - birth
+// document.write("My age is :" + " " + age)
+
+
+
+//..question 12..//
+// var radius = 20
+// var pie = 3.142
+
+// document.write("<h1> The Geometrizer</h1>")
+// document.write("Radius of circle is :" + " " + radius + "<br />")
+// document.write("The circumference is :" + " " + 2 * pie * radius + "<br />")
+// document.write("The area is :" + " " + 2 * pie * radius * 10)
+
+
+
+
+//..question 13 ..//
+// var snack = "chocolate chip";
+// var currentAge = 15
+// var maximumAge = 65
+// var snackAmount = 3
+
+// var total = (maximumAge - currentAge) * 3
+// document.write("<h1> The lifetime supply calculator </h1>")
+// document.write("Favourite snack :" + " " + snack + "<br />")
+// document.write("Current Age :" + " " + currentAge + "<br />")
+// document.write("Extimated maximum age :" + " " + maximumAge + "<br />")
+// document.write("Amount of snacks per day :" + " " + snackAmount + "<br />")
+// document.write("You will need" + " " + total + " " + "chocolate chip to last you until the ripe old age of" + " " + maximumAge)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
