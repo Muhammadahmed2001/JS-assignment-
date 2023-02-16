91d5d5f2ffa63df8491a254a069c9b7a62becf6e
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-02-16 14:39:27 +0500

    upload chapter 14-16

--- app.js
+++ app.js
@@ -1,109 +1,159 @@
 //..question 1..//
 
-// var charactar = prompt("Enter the character")
-// newCharacter = charactar.charCodeAt();
-// if (newCharacter >= 65 && newCharacter <= 90 ){
-//     alert("Capital")
-// }
-// else if(newCharacter >= 97 && newCharacter <= 122){
-//     alert("Small")
-// }
-// else{
-//     alert("Not an Alphabet")
-// }
+// var nam = new Array("ahmed","karim","samad");
+
+
 
 
 //..question 2..//
-// var num1 = +prompt("Enter the number")
-// var num2 = +prompt("Enter the second number")
 
-// if (num1 > num2){
-//     document.write( num1+ " " +"Larger number")
-// }
-// else if(num2 > num1){
-//     document.write(num2 + " " + "Larger number")
-// }
-// else if(num1 === num2){
-//     document.write("Both number are equal")
-// }
-// else{
-//     document.write("Not a number")
-// }
+// var nam = ["ahmed","karim","samad"];
+
+
 
 
 //..question 3..//
-// var num = +prompt("Enter the number")
+// var nam = ["ahmed","karim","samad"]
+// console.log(nam)
 
-// if (num > 0){
-//     document.write("Your number is Positive")
-// }
-// else if(num < 0){
-//     document.write("Your number is negative")
-// }
-// else if (num === 0){
-//     document.write("Your number is zero")
-// }
-// else{
-//     document.write("Not a number")
-// }
 
 
 //..question 4..//
-// var alphabet = prompt("Enter a alphabet")
 
-// if (alphabet === "a"||alphabet === "e"||alphabet === "i"||alphabet === "o"||alphabet === "u"){
-//     document.write("True ..! it is vowel")
-// }
-// else{
-//     document.write("False ..! Not a vowel ")
-// }
+// var num =[1,2,3,4]
+// console.log(num)
 
 
 //..question 5..//
-// var password = "ahmed";
-// var userPassword = prompt("Enter your password")
 
-// if (password === userPassword){
-//     alert("Correct!")
-// }
-// else if(userPassword === ""){
-//     alert("Please Enter Your Password")
-// }
-// else{
-//     alert("Incorrect password")
-// }
+// var boolean = [true,false,true,false];
+// console.log(boolean);
+
 
 
 //..question 6..//
+// var mix = ["ahmed",500,true];
+// console.log(mix);
 
-
-// var greeting 
-// var hour = 13
-// if (hour < 18){
-//     alert("Good day")
-// }
-// else{
-//     alert("Good evening")
-// }
 
 
 //..question 7..//
 
 
-// var time = +prompt("Enter time (24 hours clock format)");
+// var qualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.PHIL","PHD"]
 
-// if (time >= 0000 && time < 1200){
-//     document.write("<b>Good Morning!</b>")
-// }
-// else if(time >= 1200 && time < 1700 ){
-//     document.write("<b>Good Afternoon!</b>")
-// }
-// else if(time >= 1700 && time < 2100){
-//     document.write("<b>Good Evening!</b>")
-// }
-// else if (time >= 2100 && time < 2359){
-//     document.write("<b>Good Night!</b>")
-// }
-// else{
-//     document.write("<b>Something was wrong!!!</b>")
-// }
+// document.write("<h1>Qualifications</h1>")
+// document.write("<h3>"+qualification[0]+"</h3>")
+// document.write("<h3>"+qualification[1]+"</h3>")
+// document.write("<h3>"+qualification[2]+"</h3>")
+// document.write("<h3>"+qualification[3]+"</h3>")
+// document.write("<h3>"+qualification[4]+"</h3>")
+// document.write("<h3>"+qualification[5]+"</h3>")
+// document.write("<h3>"+qualification[6]+"</h3>")
+// document.write("<h3>"+qualification[7]+"</h3>")
+
+
+
+//..question 8..//
+
+
+// var nam = ["Ahmed","Karim","Samad"];
+// var score =[320,230,480];
+// var total = 500
+// var percentage_1 = score[0] * 100 / total + "%";
+// var percentage_2 = score[1] * 100 / total + "%";
+// var percentage_3 = score[2] * 100 / total + "%";
+// document.write("<b>Score of"+ " "+ nam[0]+ " " + "is" + " "+ score[0]+ "."+" "+ "Percentage :"+" "+ percentage_1 + "</b> <br />")
+// document.write("<b>Score of"+ " "+ nam[1]+ " " + "is" + " "+ score[1]+ "."+" "+ "Percentage :"+" "+ percentage_2 + "</b> <br />")
+// document.write("<b>Score of"+ " "+ nam[2]+ " " + "is" + " "+ score[2]+ "."+" "+ "Percentage :"+" "+ percentage_3 + "</b> <br />")
+
+
+
+//..question 9..//
+
+// var color = ["blue","red","green"]
+// document.write(color)
+
+//===>(a)
+// var add = prompt("Add color")
+// var add_2 = prompt("Add 2nd color")
+// color.unshift(add);
+// document.write(color)
+
+
+//===>(b)
+// color.push(add);
+// document.write(color);
+
+
+//===>(c)
+// color.unshift(add,add_2)
+// document.write(color)
+
+
+//===>(d)
+// color.shift();
+// document.write(color)
+
+
+//===>(e)
+// color.pop();
+// document.write(color);
+
+
+//===>(f)
+// var whichIndex = +prompt("Which index to add color");
+// var colorName = prompt("Color Name")
+
+// color.splice(whichIndex,0,colorName);
+
+// document.write(color)
+
+
+//===>(g)
+// var whichIndex = +prompt("Which index to delete color");
+// var color_delete = prompt("How many color you wants to delete");
+// color.splice(whichIndex,color_delete)
+// document.write(color);
+
+
+//..question 10..//
+
+// var score = [320,230,480,120];
+// score.sort();
+// document.write(score);
+
+
+
+//..question 11 ..//
+// var cities = ["Karachi","Lahore","Islamabad","Quetta","peshawar"];
+// document.write("<h1>Cities List</h1>"+ " " +cities )
+
+// var selectedCities = cities.slice(2,4);
+// document.write("<h1>Selected cities list :</h1>");
+// document.write(selectedCities)
+
+//..question 12..//
+// var arr = ["This","is","my","cat"];
+// document.write(arr.join(" "))
+
+//..question 15..//
+// var manufacture = ["Samsung","Apple","Motorola","Nokia","Sony","Haier"];
+
+// document.write("<select>"+
+// "<option>"+manufacture[0]+"</option>"+
+// "<option>"+manufacture[1]+"</option>"+
+// "<option>"+manufacture[2]+"</option>"+
+// "<option>"+manufacture[3]+"</option>"+
+// "<option>"+manufacture[4]+"</option>"+
+// "<option>"+manufacture[5]+"</option>"+
+// "</select>");
+
+
+
+
+
+
+
+
+
