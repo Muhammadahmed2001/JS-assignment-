fafc306bc5f57141bbc903877b2e2df1c868134b
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-04-27 14:17:51 +0500

    upload chapter 26-30

--- app.js
+++ app.js
@@ -18,7 +18,58 @@
 // document.write("Floor value : " + Math.floor(number) + "<br />");
 // document.write("Ceil value : " + Math.ceil(number));
 
+
+
 //..Question 3..//
 
-var number = -20.65;
-console.log(Math.abs(number));
+// var number = -5;
+// document.write("The absoljute value of " + number + " is " + Math.abs(number));
+
+
+//..Question 4..//
+
+// var random = Math.ceil(Math.random() * 6)
+// document.write("Random dice value : " + random);
+
+
+
+//..Question 5..//
+
+
+// var toss =Math.ceil(Math.random() * 2);
+// if(toss == 1){
+//     document.write("Random coins value : Heads");
+// }else{
+//     document.write("Random coins value : Tails");
+// }
+
+
+//..Question 6..//
+
+
+// var random =Math.ceil(Math.random() * 100);
+// document.write("Random number between 1 and 100 : " + random);
+
+
+
+//..Question 7..//
+
+// var weight = prompt("Enter your weight in kilograms");
+// var number = Number(weight);
+// document.write(number);
+
+
+//..Question 8..//
+
+// var userNumber = prompt("Enter a number betweem 1 and 10",2)
+// var random =  Math.ceil(Math.random() * 10);
+// if(userNumber == random){
+//     document.write("Congratulate")
+// }else{
+    
+//     document.write("Try again")
+// }
+
+
+
+
